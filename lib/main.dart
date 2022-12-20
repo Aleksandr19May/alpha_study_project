16cc2ed91427cc7a31f9a525e31f9ebc2f7ba2fa
Aleksandr19May <dela737@mail.ru>
2022-12-20 19:32:14 +0400

    add some functions

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:alpha_study_project/page2.dart';
 import 'package:alpha_study_project/settings.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
- // мой коммит
+// мой коммит
 
-
- int a = 6;
+int a = 6;
 void main() {
   runApp(const MyApp());
 }
@@ -15,12 +14,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: const Page1(),
       routes: {
-        '/Page2':(context) => Page2(),
-        '/Page3':(context) => Page3(),
+        '/Page2': (context) => Page2(),
+        '/Page3': (context) => Page3(),
       },
     );
   }
@@ -61,7 +60,9 @@ class Page1 extends StatelessWidget {
                               child: const Text(
                                 'Activity',
                                 style: TextStyle(
-                                    fontSize: 16, color: Colors.white, fontFamily: 'Gilroy-Black'),
+                                    fontSize: 16,
+                                    color: Colors.white,
+                                    fontFamily: 'Gilroy-Black'),
                               ),
                             ),
                             Container(
@@ -71,15 +72,16 @@ class Page1 extends StatelessWidget {
                               decoration: BoxDecoration(
                                   borderRadius: BorderRadius.circular(10),
                                   color: Colors.white),
-                              child:  GestureDetector (
+                              child: GestureDetector(
                                 onTap: () {
-                                   Navigator.pushNamed(context, '/Page2');
+                                  Navigator.pushNamed(context, '/Page2');
                                 },
                                 child: const Text(
                                   'Saved',
                                   style: TextStyle(
                                       fontSize: 16,
-                                      color: Color.fromARGB(255, 174, 166, 166), fontFamily: 'Gilroy-Black'),
+                                      color: Color.fromARGB(255, 174, 166, 166),
+                                      fontFamily: 'Gilroy-Black'),
                                 ),
                               ),
                             ),
@@ -93,7 +95,9 @@ class Page1 extends StatelessWidget {
                             borderRadius: BorderRadius.circular(10),
                             color: Colors.white),
                         child: IconButton(
-                          onPressed: () {Navigator.pushNamed(context, '/Page3');},
+                          onPressed: () {
+                            Navigator.pushNamed(context, '/Page3');
+                          },
                           icon: const Icon(
                             Icons.menu,
                             color: Colors.blue,
@@ -103,11 +107,10 @@ class Page1 extends StatelessWidget {
                       ),
                     ],
                   ),
-                  
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                     const  Counter(),
+                      const Counter(),
                     ],
                   ),
                   Row(
@@ -158,12 +161,14 @@ class Page1 extends StatelessWidget {
                                           'Last saved dhikrs',
                                           style: TextStyle(
                                               fontWeight: FontWeight.w700,
-                                              fontSize: 14, fontFamily: 'Gilroy-Black'),
+                                              fontSize: 14,
+                                              fontFamily: 'Gilroy-Black'),
                                         )
                                       ],
                                     ),
                                     Container(
-                                      margin: const EdgeInsets.fromLTRB(1, 1, 250, 1),
+                                      margin: const EdgeInsets.fromLTRB(
+                                          1, 1, 250, 1),
                                       width: 65,
                                       height: 2,
                                       color: const Color.fromARGB(
@@ -188,21 +193,24 @@ class Page1 extends StatelessWidget {
                                           color:
                                               Color.fromARGB(255, 86, 137, 255),
                                           fontSize: 20,
-                                          fontWeight: FontWeight.w700, fontFamily: 'Gilroy-Black'),
+                                          fontWeight: FontWeight.w700,
+                                          fontFamily: 'Gilroy-Black'),
                                     ),
                                     Text(
                                       'Name of the file dhikr',
                                       style: TextStyle(
                                           fontSize: 12,
                                           color: Colors.black,
-                                          fontWeight: FontWeight.w400, fontFamily: 'Gilroy-Black'),
+                                          fontWeight: FontWeight.w400,
+                                          fontFamily: 'Gilroy-Black'),
                                     ),
                                     Text(
                                       '19.02.2021',
                                       style: TextStyle(
                                           fontSize: 12,
                                           color: Colors.black,
-                                          fontWeight: FontWeight.w400, fontFamily: 'Gilroy-Black'),
+                                          fontWeight: FontWeight.w400,
+                                          fontFamily: 'Gilroy-Black'),
                                     ),
                                     Icon(
                                       Icons.more_horiz,
@@ -227,21 +235,24 @@ class Page1 extends StatelessWidget {
                                           color:
                                               Color.fromARGB(255, 86, 137, 255),
                                           fontSize: 20,
-                                          fontWeight: FontWeight.w700, fontFamily: 'Gilroy-Black'),
+                                          fontWeight: FontWeight.w700,
+                                          fontFamily: 'Gilroy-Black'),
                                     ),
                                     Text(
                                       'Name of the file dhikr',
                                       style: TextStyle(
                                           fontSize: 12,
                                           color: Colors.black,
-                                          fontWeight: FontWeight.w400, fontFamily: 'Gilroy-Black'),
+                                          fontWeight: FontWeight.w400,
+                                          fontFamily: 'Gilroy-Black'),
                                     ),
                                     Text(
                                       '19.02.2021',
                                       style: TextStyle(
                                           fontSize: 12,
                                           color: Colors.black,
-                                          fontWeight: FontWeight.w400, fontFamily: 'Gilroy-Black'),
+                                          fontWeight: FontWeight.w400,
+                                          fontFamily: 'Gilroy-Black'),
                                     ),
                                     Icon(
                                       Icons.more_horiz,
@@ -266,21 +277,24 @@ class Page1 extends StatelessWidget {
                                           color:
                                               Color.fromARGB(255, 86, 137, 255),
                                           fontSize: 20,
-                                          fontWeight: FontWeight.w700, fontFamily: 'Gilroy-Black'),
+                                          fontWeight: FontWeight.w700,
+                                          fontFamily: 'Gilroy-Black'),
                                     ),
                                     Text(
                                       'Name of the file dhikr',
                                       style: TextStyle(
                                           fontSize: 12,
                                           color: Colors.black,
-                                          fontWeight: FontWeight.w400, fontFamily: 'Gilroy-Black'),
+                                          fontWeight: FontWeight.w400,
+                                          fontFamily: 'Gilroy-Black'),
                                     ),
                                     Text(
                                       '19.02.2021',
                                       style: TextStyle(
                                           fontSize: 12,
                                           color: Colors.black,
-                                          fontWeight: FontWeight.w400, fontFamily: 'Gilroy-Black'),
+                                          fontWeight: FontWeight.w400,
+                                          fontFamily: 'Gilroy-Black'),
                                     ),
                                     Icon(
                                       Icons.more_horiz,
@@ -307,68 +321,93 @@ class Counter extends StatefulWidget {
 }
 
 class _CounterState extends State<Counter> {
+  int counter = 0;
+
   @override
   Widget build(BuildContext context) {
     return Container(
       height: 200,
       width: 370,
       decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          color: Colors.white),
+          borderRadius: BorderRadius.circular(10), color: Colors.white),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          Container(
-            height: 40,
-            width: 40,
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color:
-                    const Color.fromARGB(255, 86, 137, 255)),
-            child: IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                  Icons.remove,
-                  color: Colors.white,
-                )),
+          Material(
+            borderRadius: BorderRadius.circular(10),
+            color: const Color.fromARGB(255, 86, 137, 255),
+            child: InkWell(
+              child: Container(
+                height: 40,
+                width: 40,
+                child: IconButton(
+                    onPressed: () {
+                      if (counter<=0) {
+                        counter=0;
+                      } else {
+                      counter--;}
+                      setState(() {});
+                    },
+                    icon: const Icon(
+                      Icons.remove,
+                      color: Colors.white,
+                    )),
+              ),
+            ),
           ),
-          
-          Container(
-              height: 130,
-              width: 150,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color:
-                      const Color.fromARGB(255, 57, 92, 220)),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  Text('33',
-                      style: TextStyle(
-                          fontSize: 45,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white)),
-                  Text(
-                    'Dhikr',
-                    style: TextStyle(color: Colors.white),
-                  ),
+          Material(
+            borderRadius: BorderRadius.circular(10),
+            color: const Color.fromARGB(255, 57, 92, 220),
+            child: InkWell(
+              onTap: () {
+                counter++;
+                
+                
+                setState(() {
                   
-                ],
-              )),
-          Container(
-            height: 40,
-            width: 40,
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color:
-                    const Color.fromARGB(255, 86, 137, 255)),
-            child: IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                  Icons.replay,
-                  color: Colors.white,
-                )),
+                });
+              },
+              child: SizedBox(
+                  height: 150,
+                  width: 150,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(counter.toString(),
+                          style: const TextStyle(
+                              fontSize: 45,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white)),
+                      const Text(
+                        'Dhikr',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ],
+                  )),
+            ),
           ),
+          Material(
+              borderRadius: BorderRadius.circular(10),
+              color: const Color.fromARGB(255, 86, 137, 255),
+              child: InkWell(
+                
+                child: SizedBox(
+                  height: 40,
+                  width: 40,
+                  child: IconButton(
+                      onPressed: () {
+                        counter=0;
+                        
+                        setState(() {
+                          
+                        });
+                      },
+                      icon: const Icon(
+                        Icons.replay,
+                        color: Colors.white,
+                      )),
+                ),
+              )),
         ],
       ),
     );
